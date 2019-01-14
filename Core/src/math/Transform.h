8d0b2e0bfb2cd5326b38cec05e0394a55d180fbc
AXEL-STATIONAR\Axel <aandersson.axel@gmail.com>
2019-01-14 22:59:04 +0100

    Added Move, Rotate and Scale functions to Transform.

--- Core/src/math/Transform.h
+++ Core/src/math/Transform.h
@@ -22,6 +22,21 @@ namespace core {
 	public:
 		Transform();
 
+		void Move(float x, float y, float z);
+		void Move(const vec3& vec);
+		void Move(const vec4& vec);
+
+		void Rotate(float x, float y, float z);
+		void Rotate(const vec3& vec);
+		void Rotate(const vec4& vec);
+		void RotateX(float radians);
+		void RotateY(float radians);
+		void RotateZ(float radians);
+
+		void Scale(float x, float y, float z);
+		void Scale(const vec3& vec);
+		void Scale(const vec4& vec);
+
 		mat4 GetTransformationMatrix() const;
 		mat4 GetInverseTransformationMatrix() const;
 
