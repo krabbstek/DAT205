8541017c6d0e59b0fcc73d5937c9e96691eec5db
krabbstek <18243170+krabbstek@users.noreply.github.com>
2024-03-23 22:54:02 +0100

    Added functions for setting texture parameters in GLTexture2D.

--- Core/src/graphics/opengl/GLTexture2D.h
+++ Core/src/graphics/opengl/GLTexture2D.h
@@ -15,6 +15,13 @@ namespace core {
 		void Load(GLuint openGLFormat, const unsigned char* data, unsigned int width, unsigned int height, GLuint dataFormat, GLuint type);
 		bool LoadFromFile(const char* filePath);
 
+		void SetWrapS(GLuint parameter) const;
+		void SetWrapT(GLuint parameter) const;
+		void SetWrapST(GLuint parameter) const;
+		void SetMinFilter(GLuint parameter) const;
+		void SetMagFilter(GLuint parameter) const;
+		void SetMinMagFilter(GLuint parameter) const;
+
 		void Bind(unsigned int slot = 0) const;
 		static void Unbind();
 
