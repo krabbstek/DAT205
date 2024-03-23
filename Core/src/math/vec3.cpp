022305537590eaa5e98480e3509a5833b8e191f2
krabbstek <18243170+krabbstek@users.noreply.github.com>
2024-03-23 22:53:55 +0100

    Fixed std::ostream& operators.

--- Core/src/math/vec3.cpp
+++ Core/src/math/vec3.cpp
@@ -366,7 +366,7 @@ namespace core {
 		return ss.str();
 	}
 
-	std::ostream& CORE_API operator<<(std::ostream& os, const vec3& vec)
+	CORE_API std::ostream& operator<<(std::ostream& os, const vec3& vec)
 	{
 		return os << "(" << vec.x << ", " << vec.y << ", " << vec.z << ")";
 	}
