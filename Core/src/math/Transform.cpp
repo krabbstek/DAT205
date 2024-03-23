0fe8c89bbaf903e63410cd6b5627138bf1b45b0e
krabbstek <18243170+krabbstek@users.noreply.github.com>
2024-03-23 22:54:18 +0100

    Added Move, Rotate and Scale functions to Transform.

--- Core/src/math/Transform.cpp
+++ Core/src/math/Transform.cpp
@@ -10,6 +10,87 @@ namespace core {
 	}
 
 
+	void Transform::Move(float x, float y, float z)
+	{
+		position.x += x;
+		position.y += y;
+		position.z += z;
+	}
+
+	void Transform::Move(const vec3& vec)
+	{
+		position.x += vec.x;
+		position.y += vec.y;
+		position.z += vec.z;
+	}
+
+	void Transform::Move(const vec4& vec)
+	{
+		position.x += vec.x;
+		position.y += vec.y;
+		position.z += vec.z;
+	}
+
+
+	void Transform::Rotate(float x, float y, float z)
+	{
+		rotation.x += x;
+		rotation.y += y;
+		rotation.z += z;
+	}
+
+	void Transform::Rotate(const vec3& vec)
+	{
+		rotation.x += vec.x;
+		rotation.y += vec.y;
+		rotation.z += vec.z;
+	}
+
+	void Transform::Rotate(const vec4& vec)
+	{
+		rotation.x += vec.x;
+		rotation.y += vec.y;
+		rotation.z += vec.z;
+	}
+
+	void Transform::RotateX(float radians)
+	{
+		rotation.x += radians;
+	}
+
+	void Transform::RotateY(float radians)
+	{
+		rotation.y += radians;
+	}
+
+	void Transform::RotateZ(float radians)
+	{
+		rotation.z += radians;
+	}
+
+
+	void Transform::Scale(float x, float y, float z)
+	{
+		scale.x *= x;
+		scale.y *= y;
+		scale.z *= z;
+	}
+
+	void Transform::Scale(const vec3& vec)
+	{
+		scale.x *= vec.x;
+		scale.y *= vec.y;
+		scale.z *= vec.z;
+	}
+
+	void Transform::Scale(const vec4& vec)
+	{
+		scale.x *= vec.x;
+		scale.y *= vec.y;
+		scale.z *= vec.z;
+	}
+
+
 	mat4 Transform::GetTransformationMatrix() const
 	{
 		mat4 t = mat4::Translate(position);
