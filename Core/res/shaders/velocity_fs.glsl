ecc092ef53555aae4077db25e13d6703c996103a
krabbstek <18243170+krabbstek@users.noreply.github.com>
2024-03-23 22:54:03 +0100

    Added GLFramebuffer class.

---
+++ Core/res/shaders/velocity_fs.glsl
@@ -0,0 +1,11 @@
+#version 410 core
+
+in vec4 pos;
+in vec4 prevPos;
+
+out vec4 out_Color;
+
+void main()
+{
+	out_Color = 15 * (pos - prevPos);
+}
---
+++ Core/src/graphics/opengl/GLFramebuffer.cpp
@@ -0,0 +1,103 @@
+#include "pch.h"
+
+#include "GLFramebuffer.h"
+
+#include "GLCommon.h"
+
+namespace core {
+
+	GLFramebuffer::GLFramebuffer(unsigned int width, unsigned int height)
+		: m_Width(width), m_Height(height)
+	{
+		GLCall(glGenFramebuffers(1, &m_FramebufferRendererID));
+
+		if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+		{
+			CORE_ERROR("Failed to generate framebuffer!");
+			__debugbreak();
+		}
+	}
+
+	GLFramebuffer::~GLFramebuffer()
+	{
+		if (m_FramebufferRendererID)
+		{
+			GLCall(glDeleteFramebuffers(1, &m_FramebufferRendererID));
+		}
+		if (m_DepthStencilRenderbufferID)
+		{
+			GLCall(glDeleteRenderbuffers(1, &m_DepthStencilRenderbufferID));
+		}
+	}
+
+
+	GLTexture2D* GLFramebuffer::AttachRGBColorTexture() const
+	{
+		GLint fbDim[4];
+		GLCall(glGetIntegerv(GL_VIEWPORT, fbDim));
+
+		GLTexture2D* texture = new GLTexture2D();
+		texture->Load(GL_RGB, NULL, /*width =*/ fbDim[2], /*height =*/ fbDim[3], GL_RGB, GL_UNSIGNED_BYTE);
+		texture->SetMinMagFilter(GL_LINEAR);
+
+		Bind();
+		GLCall(glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, texture->m_RendererID, 0));
+
+		if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+		{
+			CORE_ERROR("Failed to attach color buffer!");
+			__debugbreak();
+		}
+
+		return texture;
+	}
+
+
+	void GLFramebuffer::GenerateDepthStencilRenderbuffer()
+	{
+		if (m_DepthStencilRenderbufferID)
+		{
+			GLCall(glDeleteRenderbuffers(1, &m_DepthStencilRenderbufferID));
+		}
+
+		GLint fbDim[4];
+		GLCall(glGetIntegerv(GL_VIEWPORT, fbDim));
+
+		GLCall(glGenRenderbuffers(1, &m_DepthStencilRenderbufferID));
+		GLCall(glBindRenderbuffer(GL_RENDERBUFFER, m_DepthStencilRenderbufferID));
+		GLCall(glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, /*width =*/ fbDim[2], /*height =*/ fbDim[3]));
+
+		Bind();
+		glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, m_DepthStencilRenderbufferID);
+
+		if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+		{
+			CORE_ERROR("Failed to generate depth stencil buffer!");
+			__debugbreak();
+		}
+	}
+
+
+	void GLFramebuffer::ClearColor(float r, float g, float b, float a) const
+	{
+		Bind();
+		GLCall(glClearColor(r, g, b, a));
+	}
+
+	void GLFramebuffer::Clear(GLuint mask)
+	{
+		GLCall(glClear(mask));
+	}
+
+
+	void GLFramebuffer::Bind() const
+	{
+		GLCall(glBindFramebuffer(GL_FRAMEBUFFER, m_FramebufferRendererID));
+	}
+
+	void GLFramebuffer::Unbind()
+	{
+		GLCall(glBindFramebuffer(GL_FRAMEBUFFER, 0));
+	}
+
+}
