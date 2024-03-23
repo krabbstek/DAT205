aaff259676a9eaa525ac61290eb6c18ceb612eef
krabbstek <18243170+krabbstek@users.noreply.github.com>
2024-03-23 22:54:18 +0100

    Added Camera component class.

---
+++ Core/src/graphics/Camera.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "Common.h"
+
+#include "entity/Component.h"
+
+#include "math/mat4.h"
+
+namespace core {
+
+	class CORE_API Camera : public Component
+	{
+	public:
+		Camera();
+
+		void GenerateViewMatrix();
+
+		inline void SetPerspective(float fov, float aspectRatio, float near, float far) { m_ProjectionMatrix = mat4::Perspective(fov, aspectRatio, near, far); }
+		inline void SetOrthographic(float left, float right, float bottom, float top, float near, float far) { m_ProjectionMatrix = mat4::Orthographic(left, right, bottom, top, near, far); }
+
+		inline const mat4& GetProjectionMatrix() const { return m_ProjectionMatrix; }
+
+		inline virtual ComponentType* GetComponentType() const { return GetComponentTypeStatic(); }
+		static ComponentType* GetComponentTypeStatic();
+
+	private:
+		mat4 m_ProjectionMatrix;
+		mat4 m_ViewMatrix;
+	};
+
+}
